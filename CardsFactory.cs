6f6a57ba952b4b518ffd5004f13245d51456d91e
Tarek <alabbar.tarek@gmail.com>
2022-11-15 00:15:48 +0000

    move const to class & add DrawCard

--- CardsFactory.cs
+++ CardsFactory.cs
@@ -2,60 +2,78 @@ namespace CoonCan;
 
 public class CardsFactory
 {
+	//TODO: Card should be an object or model.
+	//TODO: 2 classes: card class and deck class.
+	
 	//Playing cards ranks and suits.
 	private readonly string[] _ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
-	private readonly string[] _suits = { "Spades", "Hearts", "Clubs", "Diamonds"};
+	private readonly string[] _suits = { "H", "D", "C", "S"};
+	const int NumberOfDecks = 2;
+	const int NumberOfJokers = 2;
+	const int NumberOfHands = 4;
+	const int NumberOfCardPerHand = 14;
+	public List<string> ShuffledDeck = new List<string>();
+	public string Ground = "";
 
-	
+
 	//Iterates through the ranks and suits lists, to generate all of the cards and add them to a list.
 	//Then returns a shuffled deck.
-	public List<string> GenerateShuffledDeck(int numberOfDecks, int numberOfJokers)
+	public List<string> GenerateShuffledDeck()
 	{
-		List<string> deck = new List<string>();
+		// List<string> deck = new List<string>();
 
-		for (int i = 0; i < numberOfDecks; i++)
+		for (int i = 0; i < NumberOfDecks; i++)
 		{
 			foreach (var suit in _suits)
 			{
 				foreach (var rank in _ranks)
 				{
-					deck.Add(rank + "-" + suit);
+					ShuffledDeck.Add(rank + suit);
 				}
 			}
 		}
 		
-		for (int i = 0; i < numberOfJokers; i++)
+		for (int i = 0; i < NumberOfJokers; i++)
 		{
-			deck.Add("JK");
+			ShuffledDeck.Add("JK");
 		}
 		
 		Random random = new Random();
-		deck  = deck.OrderBy(_ => random.Next()).ToList();
-		return deck;
+		ShuffledDeck  = ShuffledDeck.OrderBy(_ => random.Next()).ToList();
+		return ShuffledDeck;
 	}
 	
 	
 	// Generates a list of N number of cards from a shuffledDeck then remove the cards from shuffled deck.
-	public List<string> GenerateHand(List<string> shuffledDeck, int numberOfCardsPerHand)
+	public List<string> GenerateHand()
 	{
-		var hand = new List<string>(shuffledDeck.Take(numberOfCardsPerHand));
-		shuffledDeck.RemoveRange(0, numberOfCardsPerHand);
+		var hand = new List<string>(ShuffledDeck.Take(NumberOfCardPerHand));
+		ShuffledDeck.RemoveRange(0, NumberOfCardPerHand);
 		return hand;
 	}
 
 	//Generates a list of hands.
-	public List<List<string>> GenerateAllHands(int numberOfDecks, int numberOfJokers, int numberOfCardPerHand, int numberOfHands)
+	public List<List<string>> GenerateAllHands()
 	{
 		//Generate shuffled deck
-		List<string> _shuffledDeck = GenerateShuffledDeck(numberOfDecks, numberOfJokers);
+		List<string> shuffledDeck = GenerateShuffledDeck();
 		
 		List<List<string>> allHands = new List<List<string>>();
 		
-		for (int i = 0; i < numberOfHands; i++)
+		for (int i = 0; i < NumberOfHands; i++)
 		{
-			allHands.Add(new List<string>(GenerateHand(_shuffledDeck, numberOfCardPerHand)));
+			allHands.Add(new List<string>(GenerateHand()));
 		}
 		return allHands;
 	}
 
+	//Draw a card.
+	public void DrawCard(List<string> hand, List<string> shuffledDeck)
+	{
+		var card = shuffledDeck[0];
+		hand.Add(card);
+		shuffledDeck.RemoveAt(0);
+
+	}
+	
 }
--- Program.cs
+++ Program.cs
@@ -3,30 +3,66 @@ using System.Linq;
 
 class Program
 {
-	static void Main(string[] args)
+	 static CardsFactory cardsFactory = new CardsFactory();
+	static string PrintHand(List<string> hand)
 	{
-		CardsFactory cardsFactory = new CardsFactory();
-		int numberOfDecks = 2;
-		int numberOfJokers = 2;
-		int numberOfHands = 4;
-		int numberOfCardPerHand = 14;
-	
-
-		//Call a method to generate all of hands
-		List<List<string>> allHands =
-			cardsFactory.GenerateAllHands(numberOfDecks, numberOfJokers, numberOfCardPerHand, numberOfHands);
-
-		//Debugging
-		foreach (var hand in allHands)
+		string cards = "";
+		// Console.Write("Hand: ");
+		foreach (var card in hand)
 		{
-			Console.Write("Hand: ");
-			foreach (var card in hand)
-			{
-				Console.Write(card + ", ");
-			}
-			Console.WriteLine();
+			cards += card + "\t";
 		}
 
+		return cards;
+	}
 
+	static void PrintShuffledDeck()
+	{
+		Console.WriteLine("ShuffledDeck Size= " + cardsFactory.ShuffledDeck.Count);
+		Console.WriteLine("Ground card is: " + cardsFactory.Ground);
+		// foreach (var card in cardsFactory.ShuffledDeck)
+		// {
+		// 	Console.Write(card + "\t");
+		// }
+		
+	}
+	static void Main(string[] args)
+	{
+
+		//Call a method to generate all of hands
+		List<List<string>> allHands = cardsFactory.GenerateAllHands();
+
+		//Debugging
+		// foreach (var hand in allHands)
+		// {
+		// 	PrintHand(hand);
+		// }
+		//
+		// PrintShuffledDeck();
+		//
+		// foreach (var hand in allHands)
+		// {
+		// 	cardsFactory.DrawCard(hand, cardsFactory.ShuffledDeck);
+		// }
+		//
+		// foreach (var hand in allHands)
+		// {
+		// 	PrintHand(hand);
+		// }
+		//
+		// PrintShuffledDeck();
+
+		for (int i = 0; i < 4; i++)
+		{ 
+			Console.WriteLine("(Before draw card) Player{0}: {1}", i+1, PrintHand(allHands[i]));
+			cardsFactory.DrawCard(allHands[i], cardsFactory.ShuffledDeck);
+			Console.WriteLine("(After draw card) Player{0}: {1}", i+1, PrintHand(allHands[i]));
+			Console.Write("Play a card:");
+			var userInput = Console.ReadLine();
+			allHands[i].Remove(userInput);
+			cardsFactory.Ground = userInput;
+			Console.WriteLine("Player{0}: {1}", i+1, PrintHand(allHands[i]));
+			PrintShuffledDeck();
+		}
 	}
 }
